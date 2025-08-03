99c7ec068dd1f7a6e5ba2763ab476b7545df6d10
Dawar <kdawar0102@gmail.com>
2025-08-03 23:59:59 +0530

    last line initialized

---
+++ src/assets/components/LastLine.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function LastLine() {
+  return (
+    <section>
+        <div>
+            <p></p>
+            <ul>
+                <li><img src="" alt="" /></li>
+                <li><img src="" alt="" /></li>
+                <li><img src="" alt="" /></li>
+                <li><img src="" alt="" /></li>
+                <li><img src="" alt="" /></li>
+            </ul>
+        </div>
+    </section>
+  )
+}
